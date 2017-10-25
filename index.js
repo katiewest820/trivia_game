e31259ac3e104c05fde02e7a365a585f3e8c2e9c
Katie West <katiewest820@gmail.com>
2017-10-24 17:49:05 -0700

    added_counter_for_answers_and_loop_for_new_game

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let sessionToken; 
 let questionInfo = 0;
 let apiInfo;
+let ua;
+//let correct;
+let score = 0;
 let categories = {
 	gn: 9,
 	enB: 10, 
 	enF: 11,
 	enM: 12,
-	enMTh: 13,
+	//enMTh: 13,
 	enT: 14,
 	enVG: 15,
 	enBG: 16,
@@ -18,7 +21,7 @@ let categories = {
 	geo: 22,
 	hist: 23,
 	poli: 24,
-	art: 25,
+	//art: 25,
 	celeb: 26,
 	animal: 27,
 	veh: 28
@@ -46,42 +49,125 @@ function shuffleArray(array) {
 	}
 }
 
-function loadQuestion(){
-$('.button').on('click', function(){
+function callAPI(){
+	$('.button').on('click', function(){
 	let categorySelection = $('.category').val();
 
 	console.log(categories[categorySelection])
 	let categoryToken = categories[categorySelection]
 	let difficulty = $('.difficulty').val();
-	$.ajax(`https://opentdb.com/api.php?amount=10&token=${sessionToken}&category=${categoryToken}&difficulty=${difficulty}`).done(function(data) {
-		console.log(data)
 
-		answers.push(data.results[questionInfo].correct_answer, data.results[questionInfo].incorrect_answers[0], data.results[questionInfo].incorrect_answers[1], data.results[questionInfo].incorrect_answers[2]);
+		$.ajax(`https://opentdb.com/api.php?amount=10&token=${sessionToken}&category=${categoryToken}&difficulty=${difficulty}`).done(function(data) {
+			console.log(data)
+
+			
+			apiInfo = data.results;
+			console.log(apiInfo)
+			loadQuestion()
+		});	
+	});
+}
+
+function loadQuestion(){
+//$('.button').on('click', function(){
+	//let categorySelection = $('.category').val();
+
+	//console.log(categories[categorySelection])
+	//let categoryToken = categories[categorySelection]
+	//let difficulty = $('.difficulty').val();
+	//$.ajax(`https://opentdb.com/api.php?amount=10&token=${sessionToken}&category=${categoryToken}&difficulty=${difficulty}`).done(function(data) {
+		//console.log(data)
+
+		//answers.push(data.results[questionInfo].correct_answer, data.results[questionInfo].incorrect_answers[0], data.results[questionInfo].incorrect_answers[1], data.results[questionInfo].incorrect_answers[2]);
+		//console.log(answers)
+		//apiInfo = data.results;
+		//correct = apiInfo[questionInfo].correct_answer;
+		if(questionInfo <= 9){
+		answers.push(apiInfo[questionInfo].correct_answer);
+
+		for(let i = 0; i < apiInfo[questionInfo].incorrect_answers.length; i++){
+		answers.push(apiInfo[questionInfo].incorrect_answers[i])//, apiInfo[questionInfo].incorrect_answers[1], apiInfo[questionInfo].incorrect_answers[2]);
+		}
 		console.log(answers)
-		apiInfo = data.results;
+		shuffleArray(answers)
 		$('.question').html(apiInfo[questionInfo].question);
+		for(let i = 0; i < answers.length; i++){
+			$('.answers').append(`<input type="radio" value="${answers[i]}">${answers[i]}`);
+		}
 		$('.setup').fadeOut(300);
 		$('.questionPage').fadeIn(300).css('display', 'block');
-		shuffleArray(answers)
+		}
 		console.log(answers)
-	});	
-});
 }
 
 function nextQ(){
 	$('.nextQ').on('click', function(){
+		clearAnswers()
+		answers = [];
 		questionInfo = questionInfo + 1;
 		loadQuestion()
 		console.log(questionInfo)
+	});
+}
 
+function clearAnswers(){
+	$('.answers').empty()
+}
+
+function userAnswer(){
+	$('.nextQ').on('click', function(){
+		ua = $('.answers').find('input:checked').val()
+		console.log(ua)
+		correctAnswer();
+		endOfGame();
+	});
+	
+}
+
+function correctAnswer(){
+	if (ua == apiInfo[questionInfo].correct_answer){
+		$('.questionPage').fadeOut(300).delay(3000).fadeIn(300)
+		$('.lightBox').fadeIn(300).delay(3000).fadeOut(300);
+		$('.correct').fadeIn(200).delay(3000).fadeOut(300);
+		//alert('correct');
+		score = score + 1;
+	}else{
+		$('.questionPage').fadeOut(300).delay(3000).fadeIn(300)
+		$('.lightBox').fadeIn(300).delay(3000).fadeOut(300);
+		$('.incorrect').fadeIn(200).delay(3000).fadeOut(300);
+		//alert('incorrect');
+	}
+}
+
+function endOfGame(){
+	if(questionInfo == 9){
+		correct = ' ';
+		resultsPage()
+	}
+}
+
+function resultsPage(){
+	$('.questionPage').fadeOut(400);
+	$('.resultsPage').fadeIn(400)
+	$('.resultsScore').html(`Your Score is: ${score} out of 10`);
+}
+
+function playAgain(){
+	$('.playAgain').on('click', function(){
+		questionInfo = 0;
+		score = 0;
+		setup();
+		$('.resultsPage').fadeOut(400);
+		$('.setup').fadeIn(400);
 	})
 }
 
 
-
-loadQuestion()
+userAnswer()
+callAPI()
 nextQ()
 setup()
+playAgain()
 
 
 
