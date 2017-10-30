490ee9c17f8ebdc6721c10f2a0614c3a5ffe66f4
Katie West <katiewest820@gmail.com>
2017-10-30 10:34:35 -0700

    start_page_results_page_updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let sessionToken; 
 let questionInfo = 0;
 let apiInfo;
-let ua;
+let userAnsArr = [];
 //let correct;
 let score = 0;
 let categories = {
@@ -35,8 +35,12 @@ function setup(){
 			sessionToken = data.token;
 			console.log(sessionToken)
 		});
-	$('.startPage').fadeOut(300);
-	$('.setup').fadeIn(300).css('display', 'block');
+	$('.greeting').addClass('animated bounceOutRight');
+	$('.start').addClass('animated bounceOutLeft');	
+	setTimeout(function(){
+		$('.startPage').hide();
+		$('.setup').css('display', 'block');
+	}, 600);
 	});
 }
 
@@ -50,6 +54,8 @@ function shuffleArray(array) {
 }
 
 function callAPI(){
+	$('.categoryHeader').removeClass('animated bounceOutRight');
+	$('.categoryDropdowns').removeClass('animated bounceOutLeft');
 	$('.button').on('click', function(){
 	let categorySelection = $('.category').val();
 
@@ -69,45 +75,50 @@ function callAPI(){
 }
 
 function loadQuestion(){
-//$('.button').on('click', function(){
-	//let categorySelection = $('.category').val();
+	if(questionInfo <= 9){
+	answers.push(apiInfo[questionInfo].correct_answer);
 
-	//console.log(categories[categorySelection])
-	//let categoryToken = categories[categorySelection]
-	//let difficulty = $('.difficulty').val();
-	//$.ajax(`https://opentdb.com/api.php?amount=10&token=${sessionToken}&category=${categoryToken}&difficulty=${difficulty}`).done(function(data) {
-		//console.log(data)
-
-		//answers.push(data.results[questionInfo].correct_answer, data.results[questionInfo].incorrect_answers[0], data.results[questionInfo].incorrect_answers[1], data.results[questionInfo].incorrect_answers[2]);
-		//console.log(answers)
-		//apiInfo = data.results;
-		//correct = apiInfo[questionInfo].correct_answer;
-		if(questionInfo <= 9){
-		answers.push(apiInfo[questionInfo].correct_answer);
-
-		for(let i = 0; i < apiInfo[questionInfo].incorrect_answers.length; i++){
-		answers.push(apiInfo[questionInfo].incorrect_answers[i])//, apiInfo[questionInfo].incorrect_answers[1], apiInfo[questionInfo].incorrect_answers[2]);
-		}
-		console.log(answers)
-		shuffleArray(answers)
-		$('.question').html(apiInfo[questionInfo].question);
-		for(let i = 0; i < answers.length; i++){
-			$('.answers').append(`<input type="radio" value="${answers[i]}">${answers[i]}`);
-		}
+	for(let i = 0; i < apiInfo[questionInfo].incorrect_answers.length; i++){
+	answers.push(apiInfo[questionInfo].incorrect_answers[i]);
+	}
+	console.log(answers)
+	shuffleArray(answers)
+	$('.question').html(apiInfo[questionInfo].question);
+	for(let i = 0; i < answers.length; i++){
+		$('.answers').append(`<input type="radio" value="${answers[i]}">${answers[i]}`);
+	}
+	$('.categoryHeader').addClass('animated bounceOutRight');
+	$('.categoryDropdowns').addClass('animated bounceOutLeft');	
+	setTimeout(function(){
 		$('.setup').fadeOut(300);
 		$('.questionPage').fadeIn(300).css('display', 'block');
-		}
-		console.log(answers)
+	}, 600);
+	}
+	console.log(answers)
 }
 
 function nextQ(){
 	$('.nextQ').on('click', function(){
-		clearAnswers()
-		answers = [];
-		questionInfo = questionInfo + 1;
-		loadQuestion()
-		console.log(questionInfo)
+		if(ua == undefined){
+			return;
+		}else{
+			clearAnswers()
+			answers = [];
+			questionInfo = questionInfo + 1;
+			loadQuestion()
+			console.log(questionInfo)
+		}
 	});
+}
+
+function noAnswer(){
+	if(ua == undefined){
+		$('.questionPage').fadeOut(300).delay(1200).fadeIn(300);
+		$('.lightBox').fadeIn(300).delay(1000).fadeOut(300);
+		$('.noAnswer').fadeIn(200).delay(1000).fadeOut(300);
+	}else{
+		correctAnswer();
+	}
 }
 
 function clearAnswers(){
@@ -117,8 +128,12 @@ function clearAnswers(){
 function userAnswer(){
 	$('.nextQ').on('click', function(){
 		ua = $('.answers').find('input:checked').val()
-		console.log(ua)
-		correctAnswer();
+		if(ua !== undefined ){
+			userAnsArr.push(ua);
+		}
+		console.log(userAnsArr)
+		//correctAnswer();
+		noAnswer();
 		endOfGame();
 	});
 	
@@ -126,21 +141,19 @@ function userAnswer(){
 
 function correctAnswer(){
 	if (ua == apiInfo[questionInfo].correct_answer){
-		$('.questionPage').fadeOut(300).delay(3000).fadeIn(300)
-		$('.lightBox').fadeIn(300).delay(3000).fadeOut(300);
-		$('.correct').fadeIn(200).delay(3000).fadeOut(300);
-		//alert('correct');
+		//$('.questionPage').fadeOut(300).delay(1200).fadeIn(300)
+		//$('.lightBox').fadeIn(300).delay(1000).fadeOut(300);
+		//$('.correct').fadeIn(200).delay(1000).fadeOut(300);
 		score = score + 1;
-	}else{
-		$('.questionPage').fadeOut(300).delay(3000).fadeIn(300)
-		$('.lightBox').fadeIn(300).delay(3000).fadeOut(300);
-		$('.incorrect').fadeIn(200).delay(3000).fadeOut(300);
-		//alert('incorrect');
-	}
+	}//else{
+		//$('.questionPage').fadeOut(300).delay(1200).fadeIn(300)
+		//$('.lightBox').fadeIn(300).delay(1000).fadeOut(300);
+		//$('.incorrect').fadeIn(200).delay(1000).fadeOut(300);
+	//}
 }
 
 function endOfGame(){
-	if(questionInfo == 9){
+	if(questionInfo >= 9){
 		correct = ' ';
 		resultsPage()
 	}
@@ -148,15 +161,27 @@ function endOfGame(){
 
 function resultsPage(){
 	$('.questionPage').fadeOut(400);
-	$('.resultsPage').fadeIn(400)
+	$('.resultsPage').fadeIn(400);
 	$('.resultsScore').html(`Your Score is: ${score} out of 10`);
+
+	setTimeout(function(){
+		for(let i = 0; i < 10; i++){
+		$('.correctAnswers').append(`<div class="resultsQuestion">Question ${[i + 1]}: ${apiInfo[i].question}</div><div class="resultsCorrect">Correct answer: ${apiInfo[i].correct_answer}</div><div class="resultsUA">Your answer: ${userAnsArr[i]}</div>` )
+		}
+		//$('.playAgainDiv').html(`<button class="playAgain">Play Again</button>`);
+	}, 7000);
 }
 
 function playAgain(){
 	$('.playAgain').on('click', function(){
 		questionInfo = 0;
 		score = 0;
-		setup();
+		userAnsArr = [];
+		$.ajax('https://opentdb.com/api_token.php?command=request').done(function(data) {
+			sessionToken = data.token;
+			console.log(sessionToken)
+		});
+		callAPI();
 		$('.resultsPage').fadeOut(400);
 		$('.setup').fadeIn(400);
 	})
